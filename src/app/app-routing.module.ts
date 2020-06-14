f9962a7abfc4001c78d0cb58feb065fdc066b73e
locoBit <alan_herlo@hotmail.com>
2020-06-14 18:13:35 -0500

    Setting general configurations

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,38 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { FirstComponent } from './first/first.component';
+import { ChildAComponent } from './first/child-a/child-a.component';
+import { SecondComponent } from './second/second.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: 'first-component',
+        component: FirstComponent,
+        children: [
+            {
+                path: 'child-a',
+                component: ChildAComponent
+            },
+        ]
+    },
+    {
+        path: 'second-component',
+        component: SecondComponent
+    },
+    {
+        path: '',
+        redirectTo: 'first-component',
+        pathMatch: 'full'
+    },
+    {
+        path: '**',
+        component: FirstComponent
+    },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+
+// Components
 import { AppComponent } from './app.component';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+import { ChildAComponent } from './first/child-a/child-a.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        FirstComponent,
+        SecondComponent,
+        ChildAComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
