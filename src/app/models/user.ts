07a274d89d1ae17f0306bd89d4df2191e61e2611
locoBit <alan_herlo@hotmail.com>
2020-06-14 18:12:44 -0500

    Creating User and Role models

---
+++ src/app/models/user.ts
@@ -0,0 +1,11 @@
+import { Role } from './role';
+
+export class User {
+    id: string;
+    username: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+    role: Role;
+    token?: string;
+}
